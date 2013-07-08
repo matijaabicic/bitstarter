b43a54492a73a7ce288e5b7ff41125cdb66a27a3
Matija Abicic <matija.abicic@gmail.com>
2013-07-08 22:17:25 +0000

    buffer and string

--- web.js
+++ web.js
@@ -1,18 +1,21 @@
 var express = require('express');
-
+var fileReader = require('fs');
 var app = express.createServer(express.logger());
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 5000);  
-  app.set("view options", {layout: false});  //This one does the trick for rendering static html
-  app.engine('html', require('ejs').renderFile); 
-  app.use(app.router);
+//app.configure(function(){
+//  app.set('port', process.env.PORT || 5000);  
+//  app.set("view options", {layout: false});  //This one does the trick for rendering static html
+//  app.engine('html', require('ejs').renderFile); 
+//  app.use(app.router);
+//
+//});
 
-});
+var indexHTMLBuffer = new Buffer(fs.readFile('index.html'));
+var stringOutput = indexHTMLBuffer.toString('ascii',0);
 
 app.get('/', function(request, response) {
  // response.send('Hello World2!');
-    response.render('index.html');
+    response.send(stringOutput);
 });
 
 var port = process.env.PORT || 5000;
