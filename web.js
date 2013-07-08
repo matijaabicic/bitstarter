20f407ad7a4648318e712eaf8a387a02b1fd8782
Matija Abicic <matija.abicic@gmail.com>
2013-07-08 22:23:38 +0000

    async file read

--- web.js
+++ web.js
@@ -2,16 +2,10 @@ var express = require('express');
 var fileReader = require('fs');
 var app = express.createServer(express.logger());
 
-//app.configure(function(){
-//  app.set('port', process.env.PORT || 5000);  
-//  app.set("view options", {layout: false});  //This one does the trick for rendering static html
-//  app.engine('html', require('ejs').renderFile); 
-//  app.use(app.router);
-//
-//});
 
-var indexHTMLBuffer = new Buffer(fs.readFile('index.html'));
-var stringOutput = indexHTMLBuffer.toString('ascii',0);
+//var indexHTMLBuffer = new Buffer(fs.readFile('index.html'));
+
+var stringOutput = fs.readFileSync('index.html','ascii');
 
 app.get('/', function(request, response) {
  // response.send('Hello World2!');
