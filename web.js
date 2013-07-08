40edbcfadb332a19b5bbbca9e09c349d1315f542
Matija Abicic <matija.abicic@gmail.com>
2013-07-08 05:23:56 +0000

    from stackoverflow

--- web.js
+++ web.js
@@ -1,13 +1,27 @@
-var express = require('express');
+var http = require('http'),
+    fs = require('fs');
 
-var app = express.createServer(express.logger());
 
-
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
+//var express = require('express');
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//var app = express.createServer(express.logger());
+
+
+//app.get('/', function(request, response) {
+//  response.send('Hello World2!');
+//});
+
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
