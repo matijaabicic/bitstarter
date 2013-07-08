91283d49ec0d6427adc0ff306433e5193dccd6ef
Matija Abicic <matija.abicic@gmail.com>
2013-07-08 05:09:31 +0000

    web.js now reads index html

--- web.js
+++ web.js
@@ -1,6 +1,16 @@
+var http=require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 var express = require('express');
 
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
+}).listen(9615);
+
 var app = express.createServer(express.logger());
+
 
 app.get('/', function(request, response) {
   response.send('Hello World2!');
