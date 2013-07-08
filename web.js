59f2ce2c4df6e9eddd9b73b5bd4af221be3f789e
Matija Abicic <matija.abicic@gmail.com>
2013-07-08 22:26:39 +0000

    web.js

--- web.js
+++ web.js
@@ -3,9 +3,8 @@ var fileReader = require('fs');
 var app = express.createServer(express.logger());
 
 
-//var indexHTMLBuffer = new Buffer(fs.readFile('index.html'));
-
-var stringOutput = fs.readFileSync('index.html','ascii');
+//var stringOutput = fs.readFileSync('index.html','ascii');
+var stringOutput= 'Helo World2!';
 
 app.get('/', function(request, response) {
  // response.send('Hello World2!');
