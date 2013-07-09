5baafeefc01f0949612a3d541dade341b0a15be2
Matija Abicic <matija.abicic@gmail.com>
2013-07-09 03:18:38 +0000

    web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 //var stringOutput = fs.readFileSync('index.html','ascii');
 var stringOutput = '';
-fs.readFileSync('./index.html', function(err, data){
+fs.readFileSync('./index.html', 'utf8', function(err, data){
     if (err) throw err;
     stringOutput = data.toString('utf8');
 //console.log(data);
