0e906345f1561241de5ff20e3370183e7bda054d
Matija Abicic <matija.abicic@gmail.com>
2013-09-12 14:26:01 -0700

    added google analytics

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-
+//console.log("declared variables");
 var stringOutput = "";
 function readContent(callback) {
     fs.readFile("./index.html", function (err, content) {
@@ -16,10 +16,13 @@ readContent(function (content) {
 
 app.get('/', function(request, response) {
  // response.send('Hello World2!');
+ //console.log(request);
     response.send(stringOutput);
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port, function() 
+	{
+	  console.log("Listening on " + port);
+	}
+);
